418520733a86a3b385af53acf6d472e76dce1149
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-08-07 13:21:00 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,9 +6,9 @@ class MP3Importer
       @path = path
     end
 
-    def files
-      @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
-    end
+    # def files
+    #   @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
+    # end
 
     def import(list_of_filenames)
       list_of_filenames.each{ |mp3_importer.rb| Song.new_by_filename(mp3_importer.rb) }
